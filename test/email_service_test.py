05ea1779f49aa6a1edf042a6f80d754886cdea51
jjjjustinan <143037211+jjjjustinan@users.noreply.github.com>
2023-12-16 00:34:37 -0500

    Update email_service_test.py

--- test/email_service_test.py
+++ test/email_service_test.py
@@ -3,4 +3,4 @@ from app.email_service import send_email
 
 def test_send_email():
 
-    assert send_email("jja106@georgetown.edu", "YOUR REQUESTED RECIPE", "YOUR REQUESTED RECIPE") == 202
+    assert send_email() == 202
